9e8600357919aec8e77dbc85c3750097b9f8fd01
Gustavo Nobre <gustavognobre@gmail.com>
2024-05-03 15:20:29 -0300

    Email verified

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,18 +1,35 @@
 "use client";
 
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import{ BeatLoader} from "react-spinners"
 import { useSearchParams } from "next/navigation";
 import { CardWrapper } from "./card-wrapper";
+import { newVerification } from "@/actions/new-verification";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
 
 export const NewVerificationForm = () =>{
+    const[error, setError] =  useState<string | undefined>();
+    const[success, setSuccess] = useState<string|undefined>();
     const searchParams = useSearchParams();
     
     const token = searchParams.get("token");
 
     const onSubmit = useCallback(()=>{
-        console.log(token);
-    }, [token]);
+        if (success || error) return;
+        if(!token) {
+            setError("Sem token")
+            return;
+        }
+        newVerification(token)
+            .then((data) =>{
+                setSuccess(data.success);
+                setError(data.error);
+            })
+            .catch(()=>{
+                setError("Alguma coisa está errada, contate o nosso suporte")
+            })
+    }, [token, success, error]);
 
     useEffect(() =>{
         onSubmit()
@@ -25,7 +42,14 @@ export const NewVerificationForm = () =>{
             headerTitle="Confirme o E-mail"
         >
             <div className="flex items-center w-full justify-center">
-                <BeatLoader/>
+                {!success && !error &&(
+                    <BeatLoader/>                    
+                )}
+                <FormSuccess message={success}/>
+                {!success && (
+                    <FormError message={error}/> 
+                ) }
+                
             </div>
 
         </CardWrapper>
