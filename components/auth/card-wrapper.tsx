9230b564aa23bd826608dd043881d5d477bba516
Bernardo Mendes <bernardomennndes@outlook.com>
2024-05-24 22:09:11 -0300

    style: format files based on prettier

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -1,46 +1,47 @@
 "use client";
 
-import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
 import { Header } from "@/components/auth/header";
 import { Social } from "@/components/auth/social";
 import { BackButton } from "@/components/auth/back-button";
 
 interface CardWrapperProps {
-    children: React.ReactNode;
-    headerLabel: string;
-    backButtonLabel: string;
-    backButtonHref: string;
-    showSocial?: boolean;
-    headerTitle:string;
-};
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+  headerTitle: string;
+  cardspace: string;
+}
 export const CardWrapper = ({
-    children,
-    headerLabel,
-    backButtonLabel,
-    backButtonHref,
-    showSocial,
-    headerTitle,
-    cardspace
+  children,
+  headerLabel,
+  backButtonLabel,
+  backButtonHref,
+  showSocial,
+  headerTitle,
+  cardspace,
 }: CardWrapperProps) => {
-    return (
-
-        <Card className={cardspace}>
-            <CardHeader>
-                <Header title={headerTitle} label={headerLabel} />
-            </CardHeader>
-            <CardContent>
-                {children}
-            </CardContent>
-            {showSocial && (
-                <CardFooter>
-                    <Social/>
-                </CardFooter>
-            )}
-            <CardFooter>
-                <BackButton
-                label={backButtonLabel}
-                href={backButtonHref}/>
-            </CardFooter>
-        </Card>
-    );
+  return (
+    <Card className={cardspace}>
+      <CardHeader>
+        <Header title={headerTitle} label={headerLabel} />
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      {showSocial && (
+        <CardFooter>
+          <Social />
+        </CardFooter>
+      )}
+      <CardFooter>
+        <BackButton label={backButtonLabel} href={backButtonHref} />
+      </CardFooter>
+    </Card>
+  );
 };
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -5,14 +5,15 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useSearchParams } from "next/navigation";
 import { LoginSchema } from "@/schemas";
-import { 
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage } from "@/components/ui/form";
-import { CardWrapper } from "@/components/auth/card-wrapper"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { CardWrapper } from "@/components/auth/card-wrapper";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { FormError } from "@/components/form-error";
@@ -22,104 +23,99 @@ import { useState, useTransition } from "react";
 import Link from "next/link";
 
 export const LoginForm = () => {
+  const searchParams = useSearchParams();
+  const urlError =
+    searchParams.get("error") === "OAuthAccountNotLinked"
+      ? "E-mail já em uso"
+      : "";
 
-    const searchParams = useSearchParams();
-    const urlError = searchParams.get("error") === "OAuthAccountNotLinked"
-        ?"E-mail já em uso"
-        :"";
-    const [error, setError] = useState<string | undefined>("")
-    const [success, setSuccess] = useState<string | undefined>("")
-    const [isPending, startTranstion] = useTransition();
-    const form = useForm<z.infer<typeof LoginSchema>>({
-        resolver: zodResolver(LoginSchema),
-        defaultValues:{
-            email:"",
-            password:"",
-        }  
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const [isPending, startTranstion] = useTransition();
+
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    setError("");
+    setSuccess("");
+    startTranstion(() => {
+      login(values).then((data) => {
+        setError(data?.error);
+        setSuccess(data?.success);
+      });
     });
-    const onSubmit = (values: z.infer<typeof LoginSchema>) =>{
-        setError("");
-        setSuccess("");
-        startTranstion(()=>{
-            login(values)
-                .then((data) => {
-                    setError(data?.error);
-                    setSuccess(data?.success);
-                })
-        });
-    }
-    return(
-        
-            <CardWrapper
-            headerTitle="Cadatro"
-            headerLabel="Bem-vindo de volta"
-            backButtonHref="/register"
-            backButtonLabel="Não tem uma conta?"
-            cardspace="w-[400px] shadow-md"
-            showSocial
-            >
-                <Form {...form}>
-                    <form
-                        onSubmit={form.handleSubmit(onSubmit)}
-                        className="space-y-6"
-                    >
-                        <div className="space-y-4">
-                            <FormField
-                            control={form.control}
-                            name="email"
-                            render={({field})=>(
-                                <FormItem>
-                                    <FormLabel>Email</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            {...field}
-                                            disabled={isPending}
-                                            placeholder="exemplo@exemplo.com"
-                                            type="email"
-                                        />
-                                    </FormControl>
-                                    <FormMessage/>
-                                </FormItem>
-                            )}
-                            />
-                            <FormField
-                            control={form.control}
-                            name="password"
-                            render={({field})=>(
-                                <FormItem>
-                                    <FormLabel>Password</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            {...field}
-                                            disabled={isPending}
-                                            placeholder="******"
-                                            type="password"
-                                        />
-                                    </FormControl>
-                                    <Button
-                                        size="sm"
-                                        variant="link"
-                                        asChild
-                                        className="px-0 font-normal">
-                                        <Link href="/reset">
-                                            Esqueceu sua senha?
-                                        </Link>
-                                    </Button>
-                                    <FormMessage/>
-                                </FormItem>
-                            )}
-                            />
-                        </div>
-                        <FormError message={error || urlError}/>
-                        <FormSuccess message={success}/>
-                        <Button
-                            disabled={isPending}
-                            type="submit"
-                            className="w-full">
-                            Login
-                        </Button>
-                    </form>
-                </Form>
-            </CardWrapper>
-    );
+  };
+
+  return (
+    <CardWrapper
+      headerTitle="Cadatro"
+      headerLabel="Bem-vindo de volta"
+      backButtonHref="/register"
+      backButtonLabel="Não tem uma conta?"
+      cardspace="w-[400px] shadow-md"
+      showSocial
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="exemplo@exemplo.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="******"
+                      type="password"
+                    />
+                  </FormControl>
+                  <Button
+                    size="sm"
+                    variant="link"
+                    asChild
+                    className="px-0 font-normal"
+                  >
+                    <Link href="/reset">Esqueceu sua senha?</Link>
+                  </Button>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error || urlError} />
+          <FormSuccess message={success} />
+          <Button disabled={isPending} type="submit" className="w-full">
+            Login
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
 };
